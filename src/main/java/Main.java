3578869874afd9b8578644106ca366d5c4ea34ad
notarutabaga <abriannalynn0027@gmail.com>
2021-12-27 12:36:55 -0500

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("myFxml.fxml"));
+        stage.setTitle("QuickLife");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
