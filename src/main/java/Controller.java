024f9911b1ea7417732d8502c1ed269d93533efe
notarutabaga <abriannalynn0027@gmail.com>
2021-12-28 14:36:24 -0500

    improved start menu

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,6 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import javax.swing.*;
 
 public class Controller {
     public TextArea storyBox;
@@ -20,10 +24,21 @@ public class Controller {
     public int health, happiness, smarts, looks;
     public Pane startingPane;
     public TextField nameInputField;
+    public Rectangle lockScreen;
+    public Text gameName;
+    public Text notarutabaga;
 
-    public void initialize() {
+    public void initializeGame(ActionEvent event) {
+        name = nameInputField.getText();
+        nameInputField.setVisible(false);
+        lockScreen.setVisible(false);
+        gameName.setVisible(false);
+        notarutabaga.setVisible(false);
+
         age = 0;
-        health = 80; happiness = 100; smarts = 40; looks = 64;
+        health = randPercent(); happiness = randPercent(); smarts = randPercent(); looks = randPercent();
+
+        nameAgeField.setText(name + ", age " + age);
 
         healthField.setText("health: " + health + "%");
         happinessField.setText("happiness: " + happiness + "%");
@@ -33,18 +48,16 @@ public class Controller {
         storyBox.setText("AGE " + age + ":\n\tI am born.");
     }
 
+    public int randPercent() {
+        return (int)(Math.random() * 101);
+    }
+
     public void ageUpClicked(ActionEvent actionEvent) {
         age++;
         nameAgeField.setText(name + ", age " + age);
         storyBox.appendText("\n\nAGE " + age + ":");
     }
 
-    public void getPlayerName(ActionEvent actionEvent) {
-        name = nameInputField.getText();
-        nameInputField.setVisible(false);
-        startingPane.setVisible(false);
-        nameAgeField.setText(name + ", age " + age);
-    }
 
     // hi
     //HIIIII -hammy
