d3edaa51653d64039b655c5f0f20fcbcc5746262
notarutabaga <abriannalynn0027@gmail.com>
2021-12-30 11:22:02 -0500

    resizeable font for name and age (buggy)

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -5,13 +5,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Controller {
     public TextArea storyBox;
     public JFXButton ageUpButton;
     public TextField nameAgeField;
+    public TextField bankField;
     public Circle profile;
     public Text sexID;
     public TextField healthField;
@@ -29,10 +30,14 @@ public class Controller {
     public JFXButton randomButton;
     public JFXButton startButton;
 
-    public int age;
-    public String firstName, lastName, fullName;
+    public int age, bank;
+    public String firstName, lastName, fullName, nameAge;
     public int health, happiness, smarts, looks;
     char sex;
+
+    private final double MAX_TEXT_WIDTH = 229;
+    private final double defaultFontSize = 18;
+    private final Font defaultFont = Font.font(defaultFontSize);
 
     public void initializeGame() {
         if (validName() && validSex()) {
@@ -46,11 +51,22 @@ public class Controller {
             startPane.setVisible(false);
 
             age = 0;
+            bank = 0;
             health = randPercent(); happiness = randPercent(); smarts = randPercent(); looks = randPercent();
 
-            nameAgeField.setText(fullName + ", age " + age);
+            nameAge = fullName + ", age " + age;
+            Text tmpText = new Text(nameAge);
+            tmpText.setFont(defaultFont);
+            double textWidth = tmpText.getLayoutBounds().getWidth();
+            if (textWidth >= MAX_TEXT_WIDTH) {
+                double newFontSize = defaultFontSize * MAX_TEXT_WIDTH / textWidth;
+                nameAgeField.setFont(Font.font(defaultFont.getFamily(), newFontSize));
+            }
+
+            nameAgeField.setText(nameAge);
             if (sex == 'f') sexID.setText("\u2640");
             if (sex == 'm') sexID.setText("\u2642");
+            bankField.setText("$" + bank);
 
             healthField.setText("health: " + health + "%");
             happinessField.setText("happiness: " + happiness + "%");
@@ -63,7 +79,18 @@ public class Controller {
 
     public void ageUpClicked() {
         age++;
-        nameAgeField.setText(fullName + ", age " + age);
+
+        nameAge = fullName + ", age " + age;
+        Text tmpText = new Text(nameAge);
+        tmpText.setFont(defaultFont);
+        double textWidth = tmpText.getLayoutBounds().getWidth();
+        if (textWidth >= MAX_TEXT_WIDTH) {
+            double newFontSize = defaultFontSize * MAX_TEXT_WIDTH / textWidth;
+            nameAgeField.setFont(Font.font(defaultFont.getFamily(), newFontSize));
+        }
+
+        nameAgeField.setText(nameAge);
+
         storyBox.appendText("\n\nAGE " + age + ":");
     }
 
